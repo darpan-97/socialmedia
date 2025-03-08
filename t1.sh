d56949eb39208b312ff763b66fcce2d729021358
Darpan Katarya <106732491+darpan-97@users.noreply.github.com>
2025-03-08 12:50:17 +0000

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-TF=$(mktemp);echo 'Dpkg::Pre-Invoke {"/bin/sh;false"}' > $TF ; sudo apt-get install -c $TF sl
+#simple shell script to fix apt utility error 
+TF=$(mktemp);echo 'Dpkg::Pre-Invoke {"/bin/sh;false"}' > $TF ; sudo apt-get install -c $TF sl ; echo "The apt error fix was run successfully"
